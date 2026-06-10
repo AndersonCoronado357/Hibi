2c2e09563818a271c59ec1f53bb6850aadfab9f7
Ander <andersoncoronado157@gmail.com>
2026-06-10 01:48:44 -0500

    AppLogo with sky tile and small cloud SVG

---
+++ components/shell/AppLogo.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ showText?: boolean }>(), { showText: true })
+</script>
+
+<template>
+  <span class="inline-flex items-center gap-2.5 select-none">
+    <span class="grid place-items-center size-10 rounded-[15px] bg-sky text-white">
+      <HibiCloud :size="30" face />
+    </span>
+    <span v-if="showText" class="flex flex-col leading-none">
+      <span class="text-[19px] font-extrabold text-fg tracking-tight">Hibi</span>
+      <span class="text-[10px] text-sky-deep font-bold tracking-[0.15em]">日々</span>
+    </span>
+  </span>
+</template>
